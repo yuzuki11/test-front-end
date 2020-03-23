3816c5f31f2648abb5972829055cbbe5287c6b50
Cyprinus <Cyprinus12138@gmail.com>
2020-03-23 15:12:05 +0800

    init

---
+++ src/main/java/com/cyprinus/matrix/MatrixApplication.java
@@ -0,0 +1,13 @@
+package com.cyprinus.matrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyprinus/matrix/MatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyprinus.matrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
