348a8e9cf2f7e3017bb7a01ce51442bec2c330ca
Cyprinus <Cyprinus12138@gmail.com>
2020-03-24 20:00:32 +0800

    完成并测试jwt组件

--- src/test/java/com/cyprinus/matrix/MatrixApplicationTests.java
+++ src/test/java/com/cyprinus/matrix/MatrixApplicationTests.java
@@ -2,17 +2,17 @@ package com.cyprinus.matrix;
 
 import com.cyprinus.matrix.entity.MatrixUser;
 import com.cyprinus.matrix.repository.MatrixUserRepository;
+import com.cyprinus.matrix.util.JwtUtil;
 import com.sun.deploy.security.SelectableSecurityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootTest(classes = MatrixApplication.class)
 class MatrixApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
     @Autowired
     private Config config;
@@ -20,12 +20,20 @@ class MatrixApplicationTests {
     @Autowired
     private MatrixUserRepository matrixUserRepo;
 
+    @Autowired
+    private JwtUtil jwt;
+
+    @Test
+    void contextLoads() {
+    }
+
     @Test
     void testUser() {
         MatrixUser user = new MatrixUser();
         user.setName("test");
         user.setRole("student");
         //matrixUserRepo.save(user);
+        System.out.print("Current user count:");
         System.out.println(matrixUserRepo.count());
 
     }
@@ -33,6 +41,18 @@ class MatrixApplicationTests {
     @Test
     void testConfig() {
         System.out.println(config.getSecretKey());
+    }
+
+    @Test
+    void  testJWT(){
+        Map<String,String> data = new HashMap<String,String>();
+        data.put("todo","test");
+        data.put("userId","test");
+        String token = jwt.sign(data);
+        System.out.println(token);
+        Map decoded = jwt.decode(token);
+        System.out.println(jwt.decode(token));
+        assert decoded.get("aud").equals("test") && decoded.get("sub").equals("test");
     }
 
 }
