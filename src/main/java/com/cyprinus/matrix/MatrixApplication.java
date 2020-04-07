462262184cae17264b463a289690fc5a74741ca3
Cyprinus <cyprinus@qq.com>
2020-04-07 11:52:01 +0800

    整合缓存中间件Redis

--- src/main/java/com/cyprinus/matrix/MatrixApplication.java
+++ src/main/java/com/cyprinus/matrix/MatrixApplication.java
@@ -2,11 +2,13 @@ package com.cyprinus.matrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableCaching
 public class MatrixApplication {
 
 	public static void main(String[] args) {
