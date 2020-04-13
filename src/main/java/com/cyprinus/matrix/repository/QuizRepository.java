939fc6ca60897c49d9027a8b908e95fa22ae6f7d
yuzuki11 <w506642268@qq.com>
2020-04-14 02:23:02 +0800

    获取课程成绩单

--- src/main/java/com/cyprinus/matrix/repository/QuizRepository.java
+++ src/main/java/com/cyprinus/matrix/repository/QuizRepository.java
@@ -3,6 +3,7 @@ package com.cyprinus.matrix.repository;
 import com.cyprinus.matrix.entity.Lesson;
 import com.cyprinus.matrix.entity.Quiz;
 import com.cyprinus.matrix.dto.QuizDTO;
+import com.cyprinus.matrix.dto.QuizDTO2;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,4 +12,7 @@ public interface QuizRepository extends JpaRepository<Quiz, String> {
 
     List<QuizDTO> findByLessonIs(Lesson lesson);
 
+    List<Quiz> findByLesson(Lesson lesson);
+
+    QuizDTO2 findBy_id(String _id);
 }
--- src/main/java/com/cyprinus/matrix/service/TeacherService.java
+++ src/main/java/com/cyprinus/matrix/service/TeacherService.java
@@ -1,6 +1,8 @@
 package com.cyprinus.matrix.service;
 
+import com.cyprinus.matrix.dto.MatrixUserDTO;
 import com.cyprinus.matrix.dto.QuizDTO;
+import com.cyprinus.matrix.dto.QuizDTO2;
 import com.cyprinus.matrix.entity.*;
 import com.cyprinus.matrix.exception.BadRequestException;
 import com.cyprinus.matrix.exception.ForbiddenException;
@@ -119,9 +121,82 @@ public class TeacherService {
         }
     }
 
-    public HashMap<String, Object> getScore(String _id, String lessonId) throws ServerInternalException {
+    public HashMap<String, Object> getScore(String lessonId) throws ServerInternalException {
         try {
             HashMap<String, Object> data = new HashMap<>();
+            Lesson lesson =lessonRepository.getOne(lessonId);
+            List<MatrixUser> student=lesson.getStudents();
+            List<Quiz> quizzes =quizRepository.findByLesson(lesson);
+            List<Submit> submits=new ArrayList<Submit>();
+            for (Quiz q:quizzes)
+            {
+                submits.addAll(submitRepository.findAllByQuiz(q));
+            }
+            //List<Submit> submits=submitRepository.findAllByQuiz(quizzes);
+            int [][]score=new int[student.size()][quizzes.size()];
+            for (int x=0;x<student.size();x++)//赋初值
+                for (int y=0;y<quizzes.size();y++)
+                    score[x][y]=-1;
+            int i=-1,j=-1;//有新的同学是i++ 有新的quiz是j++  找学生的话对应找i 找quiz的话对应找j
+            List<MatrixUser> studentByOrder = new ArrayList<MatrixUser>();
+            List<Quiz> quizByOrder = new ArrayList<Quiz>();
+            for (Submit sub:submits)
+            {
+                int stu_location=-1,quiz_location=-1;
+            if(studentByOrder.contains(sub.getStudent()))//该学生存在
+                //找到学生的位置
+                { stu_location=studentByOrder.indexOf(sub.getStudent());}
+            else {//如果没有该学生存在的话那么添加到student表里
+                i++;studentByOrder.add(sub.getStudent()); }
+            if (quizByOrder.contains(sub.getQuiz()))//该问题已经存在
+                {  quiz_location=quizByOrder.indexOf(sub.getQuiz());}
+            else{//如果没有该问题存在的话那么添加到quiz表里
+                j++;quizByOrder.add(sub.getQuiz()); }
+            int sum=0;
+            if (sub.getScore().length!=0)//批改了
+                {
+                    for (int x=0;x<sub.getScore().length;x++)//计算总分
+                sum+=sub.getScore()[x];
+                }
+            else sum=-2;//未批改
+            //sum存放到数组中 判断stu quiz是否存在
+            if (stu_location==-1)
+            {
+                if (quiz_location==-1)
+                    score[i][j]=sum;
+                else
+                score[i][quiz_location]=sum;
+            }
+            else
+            {
+                if (quiz_location==-1)
+                    score[stu_location][j]=sum;
+                else
+                score[stu_location][quiz_location]=sum;
+            }
+            }//循环list-submit完毕
+            //查找是否有-1 学生未提交
+            for (int x=0;x<student.size();x++) {
+                for (int y = 0; y < quizzes.size(); y++)
+                {
+                    if (score[x][y]==-1)//判断quiz里面有没有这个quiz student里面有没有这个student
+                  {
+                    if (!quizByOrder.contains(quizzes.get(y)))
+                            quizByOrder.add(quizzes.get(y));
+                    if (!studentByOrder.contains(student.get(x)))
+                        studentByOrder.add(student.get(x));
+                  }
+                }
+            }
+            List<QuizDTO2> quiz=new ArrayList<QuizDTO2>();
+            List<MatrixUserDTO> stu=new ArrayList<MatrixUserDTO>();
+            for (Quiz q:quizByOrder)
+                quiz.add(quizRepository.findBy_id(q.get_id()));
+            for (MatrixUser s:studentByOrder)
+                stu.add(userRepository.findBy_id(s.get_id()));
+            data.put("quizList",quiz);
+            data.put("studentList",stu);
+            data.put("scores",score);
             return data;
         } catch (Exception e) {
             throw new ServerInternalException(e);
@@ -140,7 +215,7 @@ public class TeacherService {
             Submit submit = submitRepository.getOne(submitId);
             submit.setScore((Integer[])scores.toArray(new Integer[scores.size()]));
             submitRepository.save(submit);
-            //屎 从此开始
+            //下面开始提交错题（没测过非主观题）
             List<Problem> problems = quiz.getProblems();
             Boolean ifObjective = true;
             List<Problem> wrongproblems=new ArrayList<Problem>();
@@ -157,11 +232,10 @@ public class TeacherService {
                 for (int i = 0; i < problems.size(); i++) {
                     Problem problem = problems.get(i);
                     if (scores.get(i)!=quiz.getPoints()[i])
-                        //加错题wrongproblems
+                        //加错题
                         wrongproblems.add(problem);
                 }
             }
-            //我扔
             textBookService.addProblemInTextbook(textBook,_id,lessonId,wrongproblems);
 
         }catch (Exception e) {
